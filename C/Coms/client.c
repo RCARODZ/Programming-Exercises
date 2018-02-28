06a5544095e29633c330852f3842306d2642aaa7
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-02-27 23:17:59 -0500

    Added new exercises

---
+++ C/Coms/client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+This is Client
+*/
+
+int main(int argc, char argv[]){
+
+    /*Get IP as an Input*/
+
+    return 0;
+}
---
+++ C/Coms/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+This is Server
+*/
+
+int main(int argc, char argv[]){
+
+    /*Configure Server*/
+
+    return 0;
+}
