f6b845ef042814d93fbf1702b7eb86bb1c0c86df
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-03-06 22:45:25 -0500

    adding arguents

--- C/Coms/server.c
+++ C/Coms/server.c
@@ -7,6 +7,13 @@ This is Server
 int main(int argc, char argv[]){
 
     /*Configure Server*/
+    if(argc > 1){
+        strcmp(argv[0],'start')
+        printf('Start Server...\n')
+    }else{
+        printf('No arguments added please tell the server what to do...\n')
+        printf('server.c  [start|stop] [IP] [port]\n')
+    }
 
     return 0;
 }
