603a8f70106c93608fe6d98af6009d982a424b67
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-03-06 22:52:15 -0500

    adding argument parser

--- C/Coms/client.c
+++ C/Coms/client.c
@@ -7,6 +7,14 @@ This is Client
 int main(int argc, char argv[]){
 
     /*Get IP as an Input*/
+    if(argc > 1){
+        strcmp(argv[0], 'start');
+        printf('Starting Client...\n');
+    }else{
+        printf('No argument...\n');
+        pritnf('Client [start|stop] [IP]');
+        return 0;
+    }
 
     return 0;
 }
--- C/Coms/server.c
+++ C/Coms/server.c
@@ -8,11 +8,12 @@ int main(int argc, char argv[]){
 
     /*Configure Server*/
     if(argc > 1){
-        strcmp(argv[0],'start')
-        printf('Start Server...\n')
+        strcmp(argv[0],'start');
+        printf('Start Server...\n');
     }else{
-        printf('No arguments added please tell the server what to do...\n')
-        printf('server.c  [start|stop] [IP] [port]\n')
+        printf('No arguments added please tell the server what to do...\n');
+        printf('server.c  [start|stop] [IP] [port]\n');
+        return 0;
     }
 
     return 0;
